2a05dc8bfd2b535c266aa7b221d3d2602d684a89
DePINC Dev <dev@depinc.org>
2024-03-13 11:00:58 +0000

    Enable duration fix on testnet3.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -788,6 +788,8 @@ public:
         consensus.BHDIP010DynamicBaseItersConsumeSeconds = DYNAMIC_BASE_ITERS_CONSUME_SECONDS;
         consensus.BHDIP010DynamicBaseItersItersSecRange = { 50 * 1000, 600 * 1000 };
 
+        consensus.BHDIP009TargetDurationFixes = { { 200313, -60 } };
+
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = Consensus::BIP9Deployment::ALWAYS_ACTIVE;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
